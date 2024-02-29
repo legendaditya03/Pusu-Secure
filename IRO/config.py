9beeea7fe78fc858be9094ac11ca8d571fbdb431
legendaditya03 <147244657+legendaditya03@users.noreply.github.com>
2024-02-29 13:07:00 +0530

    Update config.py

--- IRO/config.py
+++ IRO/config.py
@@ -15,21 +15,21 @@ class Config(object):
     API_ID = 123456  # integer value, dont use "" this sign get it form my.telegram.org
     API_HASH = "" # get it form my.telegram.org
     TOKEN = ""  # get it form @botfather.
-    OWNER_ID = 6045293810  # got to @Iro_m_bot and type /id
-    OWNER_USERNAME = "shaeep43" # your telegram username
+    OWNER_ID = 6513089685  # got to @PusuXSecure_Bot and type /id
+    OWNER_USERNAME = "legend_pusu" # your telegram username
     ALLOW_CHATS = True # leave it as it is
-    BOT_USERNAME = "Iro_m_bot" # your bot username get it form @botfather
-    SUPPORT_CHAT = "iro_x_support"  # Your own group for support, do not add the @ if you dont have leave it as it is
-    UPDATES_CHANNEL = "iro_bot_support"  # Your own chsnnel for support, do not add the @ if you dont have leave it as it is
+    BOT_USERNAME = "PusuXSecure_Bot" # your bot username get it form @botfather
+    SUPPORT_CHAT = "pusu_world"  # Your own group for support, do not add the @ if you dont have leave it as it is
+    UPDATES_CHANNEL = "pusu_world01"  # Your own chsnnel for support, do not add the @ if you dont have leave it as it is
     JOIN_LOGGER =  (
-        -1001690301851
-    )  # add @Iro_m_bot in your group and type /id
+        -1002090333689
+    )  # add @PusuXSecure_Bot in your group and type /id
     EVENT_LOGS = (
-        -1001690301851
-    )  # add @Iro_m_bot in your group and type /id
+        -1002090333689
+    )  # add @PusuXSecure_Bot in your group and type /id
     ERROR_LOG = (
-        -1001690301851
-    )  # add @Iro_m_bot in your group and type /id
+        -1002090333689
+    )  # add @PusuXSecure_Bot in your group and type /id
     STRICT_GMUTE = True #to allow gmutes
     START_STICKER = "" #sticker id for start animation
     TEMP_DOWNLOAD_DIRECTORY = ". /" # dont change
@@ -49,25 +49,25 @@ class Config(object):
     INFOPIC = True
     URL = None
     SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@iro_x_support"
+    SPAMWATCH_SUPPORT_CHAT = "@pusu_world"
 
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = [6045293810]
+    DRAGONS = [6513089685]
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = [6045293810]
+    DEV_USERS = [6513089685]
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = [6045293810]
+    DEMONS = [6513089685]
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = [6045293810]
-    WOLVES = [6045293810]
-    START_IMG = "https://te.legra.ph/file/32018201f5562ffa3fcc9.jpg" #yor fav img link
+    TIGERS = [6513089685]
+    WOLVES = [6513089685]
+    START_IMG = "https://graph.org/file/ac65ca5d3c0217acefcf9.jpg" #yor fav img link
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
     STRICT_GBAN = True
-    REM_BG_API_KEY = "XHPmZZztSksyF5rqc2CRBvMa"
+    REM_BG_API_KEY = "BpFP7okACX7S4rFVywGqiRHH"
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
